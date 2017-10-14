155ec16605cbbd03d844126394f40aa5885178dc
Andrew Hamilton <andrew.hamilton@harris.com>
2017-10-14 17:02:16 -0400

    got http post working

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
-const {window} = new JSDOM('<body></body>');
-const $ = require("jquery")(window);
+const axios = require('axios');
+const jquery = require('jquery');
 const fetch = require('node-fetch');
+
 
 fetch('http://gmail.com')
   .then(function (response) {
     return response.text();
   })
   .then(function (text) {
-    console.log($(text).find('form').html());
+    const {window} = new JSDOM(text);
+    const $ = jquery(window);
+    let $form = $('form');
+    let action = $form.attr('action');
+    $form.find('input[name=Email]').val('andrewhamilton');
+    let formData = $form.serialize();
+    console.log(action);
+    console.log(formData);
+
+    axios.request({
+      url: action,
+      method: 'post',
+      data: formData,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(function (response) {
+      const {window} = new JSDOM(response.data);
+      const $ = jquery(window);
+      let $form = $('form');
+      console.log($form.html());
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
   })
   .catch(function (error) {
     console.log('Request failed', error)
